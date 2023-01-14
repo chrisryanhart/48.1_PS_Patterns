3e4a336bc765bd4c41fa3b5b09f12db493e4e757
rrghiker <chris.ryan.hart1@gmail.com>
2023-01-14 08:29:36 -0500

    finished avg pair and 2 array obj

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,25 @@
 // add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(arr,target) {
+    if(arr.length === 0) return false;
+    // determine if the target average can be made with pairs in the arr
+
+    // start at first and last element
+    let left = 0;
+    let right = arr.length-1;
+
+    while(left < right){
+        let avg = (arr[left]+arr[right])/2;
+
+        if(avg === target) return true;
+    
+        if(avg < target){
+            left ++;
+        }else{
+            right --;
+        }
+    }
+    return false;
 }
+
+
+module.exports = averagePair;
--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,19 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(arr1, arr2) {
+    let resultObj = {};
+
+    let maxLength = arr1.length > arr2.length ? arr1.length : arr2.length;
+
+    for(let i=0; i<maxLength; i++){
+        if(arr1[i]===undefined){
+            return resultObj;
+        }else if(arr2[i]===undefined){
+            resultObj[arr1[i]] = null;
+        }else{
+            resultObj[arr1[i]] = arr2[i];
+        }
+    }
+    return resultObj;
 }
+
+module.exports = twoArrayObject;
